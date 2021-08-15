e2c3fe70955de872cb72c3a404d03af9e5ab1f27
mroach <git@c.mroach.com>
2021-08-15 14:13:53 +0200

    feat: stat command, similar to ls, but for one file

---
+++ cmd/stat.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/mroach/n64-go/formatters"
+	"github.com/mroach/n64-go/rom"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var outputFormat string
+
+	var statCmd = &cobra.Command{
+		Use:     "stat",
+		Aliases: []string{"info"},
+		Short:   "Get ROM file information",
+		Args:    cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path := args[0]
+
+			info, err := rom.FromPath(path)
+			if err != nil {
+				return err
+			}
+
+			return formatters.PrintAll([]rom.RomFile{info}, outputFormat)
+		},
+	}
+
+	statCmd.Flags().StringVarP(&outputFormat, "output", "o", "text", "Output format")
+
+	rootCmd.AddCommand(statCmd)
+}
